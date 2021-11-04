8590c4037d0c2a6a0c911549b43821ccc6364fc4
parfenovaa.aa <parfenovaa.aa@yandex.ru>
2021-11-04 12:01:53 +0300

    Homework_2 BaseTest added

--- src/test/java/com/epam/tc/hw2/BaseTest.java
+++ src/test/java/com/epam/tc/hw2/BaseTest.java
@@ -2,14 +2,11 @@ package com.epam.tc.hw2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
 import org.testng.asserts.SoftAssert;
 
 public class BaseTest {
