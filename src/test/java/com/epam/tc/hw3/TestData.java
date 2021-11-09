5c54ee7c2bdf85351bee0918a14a372c1db40f11
parfenovaa.aa <parfenovaa.aa@yandex.ru>
2021-11-09 19:51:32 +0300

    Homework_3 Test data transferred to TestData

--- src/test/java/com/epam/tc/hw3/TestData.java
+++ src/test/java/com/epam/tc/hw3/TestData.java
@@ -2,19 +2,19 @@ package com.epam.tc.hw3;
 
 public class TestData {
 
-    public String title = "Home Page";
-    public String userName = "ROMAN IOVLEV";
+    public final String title = "Home Page";
+    public final String userName = "ROMAN IOVLEV";
 
     //Test data for Exercise1
-    public String[] leftMenuData = {"Home", "Contact form", "Service", "Metals & Colors", "Elements packs"};
-    public String[] topMenuData = {"HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"};
-    public String[] imgTextsData = {"To include good practices\nand ideas from successful\nEPAM project",
+    public final String[] leftMenuData = {"Home", "Contact form", "Service", "Metals & Colors", "Elements packs"};
+    public final String[] topMenuData = {"HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"};
+    public final String[] imgTextsData = {"To include good practices\nand ideas from successful\nEPAM project",
         "To be flexible and\ncustomizable",
         "To be multiplatform",
         "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"};
 
     //Test data for Exercise2
-    public String[] logCheckBoxData = {" Fire: condition changed to false",
+    public final String[] logCheckBoxData = {" Fire: condition changed to false",
         " Earth: condition changed to false",
         " Wind: condition changed to false",
         " Water: condition changed to false",
@@ -23,11 +23,11 @@ public class TestData {
         " Wind: condition changed to true",
         " Water: condition changed to true"
     };
-    public String[] logRadioData = {" metal: value changed to Selen",
+    public final String[] logRadioData = {" metal: value changed to Selen",
         " metal: value changed to Bronze",
         " metal: value changed to Gold",
         " metal: value changed to Silver"};
-    public String[] logDropdownData = {" Colors: value changed to Blue",
+    public final String[] logDropdownData = {" Colors: value changed to Blue",
         " Colors: value changed to Red",
         " Colors: value changed to Yellow",
         " Colors: value changed to Green"};
